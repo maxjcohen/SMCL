98aa3f667dfe3adde20c52845558f67fee28bdf9
Max Cohen <max.zagouri@pm.me>
2021-11-08 17:53:48 +0100

    Extend compute cost function to one dimension

--- src/metrics.py
+++ src/metrics.py
@@ -4,15 +4,36 @@ import torch
 
 
 @torch.no_grad()
-def compute_cost(model, dataloader, criteria=None):
+def compute_cost(model: callable, dataloader: torch.utils.data.DataLoader, criteria: callable=None, reduction: str="none") -> torch.Tensor:
+    """Compute given cost for the model over the given dataloader.
+
+    Iterate over the dataloader, computing the given cost (default to MSE). Results can
+    be averaged by setting the `reduction` parameter.
+
+    Parameters
+    ----------
+    model: model to evaluate.
+    dataloader: iterable over the dataset.
+    criteria: cost function. If set to `None`, defaults to `torch.nn.MSELoss`.
+    reduction: if set to `"mean"`, average the computed cost over the dataset samples.
+    Default is `"none"`.
+
+    Returns
+    -------
+    Cost tensor with shape `(n_samples x T)` if reduction is `"none"`, scalar if `"mean"`.
+    """
     criteria = criteria or torch.nn.MSELoss(reduction="none")
     running_loss = []
     for u, y in dataloader:
         u = u.transpose(0, 1)
         y = y.transpose(0, 1)
         netout = model(u)
-        running_loss.append(criteria(netout.squeeze(), y.squeeze()))
-    running_loss = torch.cat(running_loss, dim=1).mean(0).mean(-1)
+        running_loss.append(criteria(netout, y))
+    # Concatenate batch and time dimension
+    d_out = y.shape[-1]
+    running_loss = torch.cat(running_loss, dim=1).reshape(-1, d_out).mean(-1)
+    if reduction == "mean":
+        running_loss = running_loss.mean()
     return running_loss
 
 
