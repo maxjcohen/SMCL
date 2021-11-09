25bbfe8d91e7acaeb7e39b009bf88c6e144b0485
Max Cohen <max.zagouri@pm.me>
2021-11-09 12:20:02 +0100

    Add cumulative cost metric

--- src/metrics.py
+++ src/metrics.py
@@ -67,3 +67,48 @@ def compute_cost(
     if reduction == "mean":
         cost = cost.mean()
     return cost
+
+
+def cumulative_cost(
+    model: callable,
+    dataloader: torch.utils.data.DataLoader,
+    eval_time: int = 48,
+    delta: int = 48,
+    criteria: callable = "mse",  # Or str
+    reduction: str = "none",
+):
+    """Compute cost for t+1 prediction to t+delta.
+
+    Parameters
+    ----------
+    model: model to evaluate.
+    dataloader: iterable over the dataset.
+    eval_time: hide observations from this time.
+    delta: total number of timesteps without observations to aggregate.
+    criteria: cost function. If set to `None`, defaults to `torch.nn.MSELoss`.
+    reduction: if set to `"mean"`, average the computed cost over the dataset samples.
+    Default is `"none"`.
+
+    Returns
+    -------
+    Cost tensor with shape `(T, n_samples)` if reduction is `"none"`, `(T)` if `"mean"`.
+    """
+    # Define model with hidden observations
+    def model_hidden(u, y=None):
+        if y is not None:
+            y_mask = torch.zeros(y.shape, dtype=bool)
+            y_mask[eval_time : eval_time + delta] = True
+            y = y.masked_fill(y_mask, float("nan"))
+            return model(u, y)
+        return model(u)
+
+    cost = compute_cost(
+        model=model_hidden, dataloader=dataloader, criteria=criteria, reduction="none"
+    )  # .reshape(dataloader.dataset.T, -1)
+    # Select study time
+    cost = cost[eval_time : eval_time + delta]
+    # Compute cumulative average
+    cost = cost.cumsum(dim=0) / torch.arange(1, delta + 1).unsqueeze(-1)
+    if reduction == "mean":
+        cost = cost.mean(-1)
+    return cost
