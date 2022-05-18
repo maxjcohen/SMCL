f4b06ebe82d7770b7486b20f500f3e62f5acb6be
Max Cohen <max.zagouri@pm.me>
2022-05-18 13:17:34 +0200

    Add pretrain and finetune scripts

---
+++ scripts/oze/finetune.py
@@ -0,0 +1,47 @@
+import argparse
+import datetime
+
+from ozedata import OzeDataModule
+from ozedata.oze.dataset import OzeDataset
+
+from .pretrain import Experiment, LitWeekEval
+
+Experiment.exp_name = "oze_finetune"
+# Select a single week to train on
+start_day = 1 + 7
+Experiment.dataset_kwargs = {
+    "train_start": datetime.datetime(year=2021, month=5, day=start_day),
+    "val_start": datetime.datetime(year=2021, month=5, day=start_day + 7),
+}
+# Disable rolling dataset as there is only one week to train on
+Experiment.DataModule.DatasetRolling = OzeDataset
+
+
+if __name__ == "__main__":
+    args = argparse.Namespace(
+        dataset_path="datasets/data_2020_2021.csv",
+        T=24 * 7,
+        d_emb=8,
+        N=20,
+        batch_size=16,
+        num_workers=4,
+        epochs=15,
+        gpus=1,
+        load_path="checkpoints/oze_pretrain/2022_05_18__130554.ckpt",
+    )
+
+    # Train
+    exp = Experiment(args)
+    exp.datamodule.setup()
+    exp.trainer.fit(exp.litmodule, train_dataloaders=exp.datamodule.train_dataloader())
+
+    # Validate on all validation weeks
+    exp.datamodule = OzeDataModule(
+        dataset_path=args.dataset_path,
+        T=args.T,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers,
+        val_start=datetime.datetime(year=2021, month=5, day=1),
+        val_end=datetime.datetime(year=2021, month=5, day=1 + 7 * 4),
+    )
+    exp.trainer.validate(exp.litmodule, datamodule=exp.datamodule)
---
+++ scripts/oze/pretrain.py
@@ -0,0 +1,64 @@
+import argparse
+import datetime
+
+import pytorch_lightning as pl
+
+from src.litmodules import LitClassicModule
+from src.utils import aim_fig_plot_ts
+from .classic import Experiment
+
+
+class LitWeekEval(LitClassicModule):
+    def validation_step(self, batch, batch_idx):
+        u, y = batch
+        y_hat = self.model(u)
+        # Log global loss
+        loss = self.criteria(y, y_hat)
+        self.log("val_loss", loss)
+        # Log weekly loss
+        for week_idx, (week_observations, week_predictions) in enumerate(
+            zip(y.transpose(0, 1), y_hat.transpose(0, 1))
+        ):
+            sample_idx = batch_idx * y.shape[1] + week_idx + 1
+            loss = self.criteria(week_observations, week_predictions)
+            self.log(f"val_loss_week{sample_idx}", loss)
+            self.logger.experiment.track(
+                aim_fig_plot_ts(
+                    {
+                        "observations": week_observations,
+                        "predictions": week_predictions,
+                    }
+                ),
+                name=f"week-{sample_idx}",
+                epoch=self.current_epoch,
+                context={"subset": "val"},
+            )
+        return y, y_hat
+
+
+Experiment.exp_name = "oze_pretrain"
+Experiment.dataset_kwargs = {
+    "val_start": datetime.datetime(year=2021, month=5, day=1),
+    "val_end": datetime.datetime(year=2021, month=5, day=1 + 7 * 4),
+}
+Experiment.LitModule = LitWeekEval
+Experiment.monitor = "train_loss"
+
+
+if __name__ == "__main__":
+    args = argparse.Namespace(
+        dataset_path="datasets/data_2020_2021.csv",
+        T=24 * 7,
+        d_emb=8,
+        N=20,
+        batch_size=16,
+        num_workers=4,
+        epochs=10,
+        gpus=1,
+        load_path=None,
+    )
+
+    exp = Experiment(args)
+
+    exp.trainer.fit(exp.litmodule, datamodule=exp.datamodule)
+    exp.trainer.validate(exp.litmodule, datamodule=exp.datamodule, ckpt_path="best")
